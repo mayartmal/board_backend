ff65f2e0a41c149cef5b9001ea8e660548f40528
mayartmal <mayartmal@gmail.com>
2020-04-08 16:29:09 +0300

    estimation arithmetic done

--- business_objects.py
+++ business_objects.py
@@ -1,62 +1,44 @@
-class Board(object):
-
-	def __init__(self):
-		super(Board, self).__init__()
-		self.title = None
-		self.columns = None
-		self.created_at = None
-		self.created_by = None
-		self.last_updated_at = None
-		self.last_updated_by = None
-
-
-		def create_board(self, title, columns):
-			self.title = title
-			self.columns = columns
-
-		def delete_board(self):
-			print("you try to delete board")
-			pass
-
-class Card(object):
-	def __init__(self):
-		super(Card, self).__init__()
-		self.title = None
-		self.board = None
-		self.status = None
-		self.description = None
-		self.assignee = None
-		self.estimation = None
-
-
-	def create_card(self, title, board, status, descripton, assignee):
-		pass	
-
-	def set_estimation(self, estimation):
-		self.estimation = estimation
-		pass
-	
-class User(object):
-	def __init__(self, arg):
-		super(User, self).__init__()
-		self.arg = arg
-	def get_all_boards():
-			pass	
-
 class Estimation(object):
-	def __init__(self):
+	day_2_hour_coef = 8
+	week_2_hour_coef = 40
+	month_2_hour_coef = 160
+
+
+	def __init__(self, m, w, d, h):
 		super(Estimation, self).__init__()
-		self.h = None
-		self.d = None
-		self.w = None
 		self.m = None
-	
+		self.w = None
+		self.d = None
+		self.h = None
+		self.set_m(m)
+		self.set_w(w)
+		self.set_d(d)
+		self.set_h(h)
+		self._total_h = self.h + (self.d) * self.day_2_hour_coef + (self.w) * self.week_2_hour_coef + (self.m) * self.month_2_hour_coef
+
+
 	def __add__(self, additional_estimation):
-		result_estimation = Estimation()
-		result_estimation.h = self.h + additional_estimation.h
+		result_estimation = Estimation("0m", "0w", "0d", "0h")
+		result_estimation._total_h = self._total_h + additional_estimation._total_h
+		
+		result_estimation.m = result_estimation._total_h // self.month_2_hour_coef
+		hours_transfert = result_estimation._total_h % self.month_2_hour_coef
+		
+		result_estimation.w = hours_transfert // self.week_2_hour_coef
+		hours_transfert = hours_transfert % self.week_2_hour_coef
+
+		result_estimation.d = hours_transfert // self.day_2_hour_coef
+		hours_transfert = hours_transfert % self.day_2_hour_coef
+
+		result_estimation.h = hours_transfert
+		
 		return result_estimation
 
 
+	def get_estimation_value(self):
+		estimation_value = self.get_m() + self.get_w() + self.get_d() + self.get_h()
+		return estimation_value
+	
 	def set_m(self, m):
 		self.m = self.__universal_set(m, "m")
 	def get_m(self):
@@ -83,9 +65,17 @@ class Estimation(object):
 		return timed_h
 
 
+
+
 	def __universal_set(self, time_value, time_base):
 		if (str(type(time_value)) == "<class 'str'>") & ((time_value[-1] == time_base)):
 			numeric_value = int(time_value[:-1])
+			if (time_base == "h") & (numeric_value > 7):
+				raise NameError("The hour value can be greater then 7")
+			if (time_base == "d") & (numeric_value > 4):
+				raise NameError("The day value can be greater then 4")		
+			if (time_base == "w") & (numeric_value > 3):
+				raise NameError("The week value can be greater then 3")	
 			return numeric_value
 		else:
 			raise NameError("The input value must be string and have '-" + time_base + "' ending")
--- core.py
+++ core.py
@@ -2,22 +2,17 @@ import business_objects
 
 
 def main():
-	
-	#test_board = business_objects.Board("Karantin", ["Todo", "In Work", "Done"])
-	#print(test_board.title)
-	#print(test_board.columns)
+
+	es1 = business_objects.Estimation("1m", "3w", "1d", "2h")
+	es2 = business_objects.Estimation("2m", "2w", "4d", "7h")
+	es_r = es1 + es2
+
+	print(es1.get_estimation_value())
+	print(es2.get_estimation_value())
+	print(es_r.get_estimation_value())
 
 
-	estimation_task1 = business_objects.Estimation()
-	estimation_task1.set_h("1h")
-	estimation_task2 = business_objects.Estimation()
-	estimation_task2.set_h("3h")
 
-	a = estimation_task1 + estimation_task2
-	print(a.get_h())
 
-	a.set_h("11h")
-	print(a.get_h())
-	print("Good")
 
 main()			
